d4c2b9ae1b79a6d1b08a10284a48f0e6cd9215f8
UnGast <zadrian@web.de>
2020-08-07 12:30:47 +0200

    update

--- Sources/Demo/main.swift
+++ Sources/Demo/main.swift
@@ -3,15 +3,98 @@ import SDL
 import CSDL2
 import GL
 
+var vertexShaderSource = """
+#version 330 core
+
+layout (location = 0) in vec3 inPos;
+
+void main() {
+    gl_Position = vec4(inPos, 1.0);
+}
+"""
+
+let fragmentShaderSource = """
+#version 330 core
+
+layout (location = 0) out vec4 outColor;
+
+void main() {
+    outColor = vec4(0.5, 1.0, 0.5, 1.0);
+}
+"""
+
+let vertices: [GL.Float] = [
+    0, 1.0, 0.1,
+    1.0, -1.0, 0.1,
+    -1.0, -1.0, 0.1
+]
+
 func main() throws {
     try SDLSystem.initialize(subSystems: [.video])
 
-    let window = try SDLWindow(
-        title: "Swift SDL2 OpenGL Demo",
-        frame: (x: .centered, y: .centered, width: 800, height: 600),
-        options: [.resizable, .opengl])
+    let window = SDL_CreateWindow(
+        "Swift SDL2 OpenGL Demo", // title
+        Int32(SDL_WINDOWPOS_CENTERED_MASK), // x
+        Int32(SDL_WINDOWPOS_CENTERED_MASK), // y
+        800, // width
+        600, // height
+        UInt32(SDL_WINDOW_RESIZABLE.rawValue | SDL_WINDOW_OPENGL.rawValue)) // flags
 
-    let glContext = try SDLGLContext(window: window)
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3)
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3)
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, Int32(SDL_GL_CONTEXT_PROFILE_CORE.rawValue))
+    let glContext = SDL_GL_CreateContext(window)
+
+    var infoLogLength = GL.Int()
+    var infoLog = [GL.Char]()
+
+    let vertexShader = glCreateShader(GL.VERTEX_SHADER)
+    withUnsafePointer(to: vertexShaderSource) { glShaderSource(vertexShader, 1, $0, nil) }
+    glCompileShader(vertexShader)
+    glGetShaderiv(vertexShader, GL.INFO_LOG_LENGTH, &infoLogLength)
+    if infoLogLength > 0 {
+        infoLog = [GL.Char](repeating: GL.Char(0), count: Int(infoLogLength))
+        withUnsafeMutablePointer(to: &infoLog[0]) { glGetShaderInfoLog(vertexShader, infoLogLength, nil, $0) }
+        print("Vertex shader info log:", String(cString: infoLog))
+    }
+
+    let fragmentShader = glCreateShader(GL.FRAGMENT_SHADER)
+    withUnsafePointer(to: fragmentShaderSource) { glShaderSource(fragmentShader, 1, $0, nil) }
+    glCompileShader(fragmentShader)
+    glGetShaderiv(fragmentShader, GL.INFO_LOG_LENGTH, &infoLogLength)
+    if infoLogLength > 0 {
+        infoLog = [GL.Char](repeating: GL.Char(0), count: Int(infoLogLength))
+        withUnsafeMutablePointer(to: &infoLog[0]) { glGetShaderInfoLog(fragmentShader, infoLogLength, nil, $0) }
+        print("Fragment shader info log:", String(cString: infoLog))
+    }
+
+    let shaderProgram = glCreateProgram()
+    glAttachShader(shaderProgram, vertexShader)
+    glAttachShader(shaderProgram, fragmentShader)
+    glLinkProgram(shaderProgram)
+    glGetProgramiv(shaderProgram, GL.INFO_LOG_LENGTH, &infoLogLength)
+    if infoLogLength > 0 {
+        infoLog = [GL.Char](repeating: GL.Char(0), count: Int(infoLogLength))
+        withUnsafeMutablePointer(to: &infoLog[0]) { glGetProgramInfoLog(shaderProgram, infoLogLength, nil, $0) }
+        print("Program info log:", String(cString: infoLog))
+    }
+
+    glDeleteShader(vertexShader)
+    glDeleteShader(fragmentShader)
+
+    var VAO = GL.UInt()
+    glGenVertexArrays(1, &VAO)
+    glBindVertexArray(VAO)
+
+    var VBO = GL.UInt()
+    glGenBuffers(1, &VBO)
+    glBindBuffer(GL.ARRAY_BUFFER, VBO)
+    glBufferData(GL.ARRAY_BUFFER, vertices.count * MemoryLayout<GL.Float>.size, vertices, GL.STATIC_DRAW)
+
+    glVertexAttribPointer(0, 3, GL.FLOAT, false, GL.Size(MemoryLayout<GL.Float>.size * 3), nil)
+    glEnableVertexAttribArray(0)
+
+    glUseProgram(shaderProgram)
 
     var running = true
 
@@ -29,12 +112,18 @@ func main() throws {
         }
 
         glClearColor(0.2, 0.3, 1.0, 1.0)
-        glClear(GL.COLOR_BUFFER_BIT)
+        glClear(GL.DEPTH_BUFFER_BIT | GL.COLOR_BUFFER_BIT | GL.STENCIL_BUFFER_BIT)
+        glBindVertexArray(VAO)
+        glUseProgram(shaderProgram)
+        glDrawArrays(GL.TRIANGLES, 0, 3)
 
-        window.glSwap()
+        SDL_GL_SwapWindow(window)
     }
 
-    SDLSystem.quit()
+    SDL_GL_DeleteContext(glContext)
+    SDL_DestroyWindow(window)
+    SDL_VideoQuit()
+    SDL_Quit()
 }
 
 do {
